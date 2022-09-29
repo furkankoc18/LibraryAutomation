867116696bdc86593dfb361e22a36e71830c7e9c
TCABKOC <furkan.koc@turkcell.com.tr>
2022-09-29 18:30:37 +0300

    first commit

---
+++ src/main/java/com/library/automation/LibraryautomationApplication.java
@@ -0,0 +1,13 @@
+package com.library.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryautomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryautomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/automation/LibraryautomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryautomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
